2aec38d6f7567db6316f4cbf8f59c3e69b3f0edb
Abdmustaqim <59740593+Mrthief69@users.noreply.github.com>
2022-11-05 20:37:10 +0800

    Add files via upload

--- cek_login.php
+++ cek_login.php
@@ -6,32 +6,32 @@ include 'koneksi.php';
  
 $email = $_POST['email'];
 $password = $_POST['pass'];
- 
- 
-$login = mysqli_query($conn,"SELECT * from user where email='$email' and pass='$password'");
+
+$login = mysqli_query($conn,"SELECT * from user where email='$email'");
 $cek = mysqli_num_rows($login);
  
-
 if($cek > 0){
- 
 	$data = mysqli_fetch_assoc($login);
- 
-	if($data['level']=="admin"){
-		$_SESSION['email'] = $email;
-		$_SESSION['level'] = "admin";
-		header("location:./admin/index.php");
- 
-	}else if($data['level']=="user"){
-		$_SESSION['email'] = $email;
-		$_SESSION['level'] = "user";
-		header("location:mainpage.php");
- 
-	// cek jika user login sebagai pengurus
+
+	if(password_verify($password, $data['pass'])) {
+		if($data['level']=="admin"){
+			$_SESSION['email'] = $email;
+			$_SESSION['level'] = "admin";
+			header("location:./admin/index.php");
+	
+		}else if($data['level']=="user"){
+			$_SESSION['email'] = $email;
+			$_SESSION['level'] = "user";
+			header("location:mainpage.php");
+
+		}else{
+			header("location:login.php?pesan=gagal1");
+		}
 	}else{
-		header("location:login.php?pesan=gagal");
+		header("location:login.php?pesan=wrongpass");
 	}	
 }else{
-	header("location:login.php?pesan=gagal");
+	header("location:login.php?pesan=gagal2");
 }
  
 ?>
